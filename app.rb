d61b372bf6f2598cbd13b0d72a43a66e3e4b9947
noby <n.fujioka@abacusmaths.info>
2016-07-18 16:42:55 +0100

    adds several rountes

--- app.rb
+++ app.rb
@@ -3,3 +3,15 @@ require 'sinatra'
 get '/' do
   "hello!"
 end
+
+get '/secret' do
+  "This is a secrete message. You are not supposed to be here."
+end
+
+get '/hello' do
+  "Hello, World!"
+end
+
+get '/bye' do
+  "Bye, World!"
+end
